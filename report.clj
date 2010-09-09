2fd2700efabf5b98d4fec5eee9e268847f957cc1
Chouser <chouser@n01se.net>
2010-09-08 21:06:08 -0400

    Add computed field to InventoryItem

--- report.clj
+++ report.clj
@@ -1,12 +1,3 @@
-(defn inventory-item [description quantity]
-  ^{:type ::InventoryItem} {:description description, :quantity quantity})
-
-(def test-inventory-items
-  [(inventory-item "Widget" 28)
-   (inventory-item "Gizmo"  32)
-   (inventory-item "Thingy" 38)])
-
-
 (defn purchase-order [id date amount]
   ^{:type ::PurchaseOrder} {:id id, :date date, :amount amount})
 
@@ -14,6 +5,17 @@
   [(purchase-order 1 "2010-01-01" 12.99)
    (purchase-order 2 "2010-02-02" 24.98)
    (purchase-order 3 "2010-03-03" 18.85)])
+
+
+(defn inventory-item [description quantity cost]
+  ^{:type ::InventoryItem} {:description description, :quantity quantity,
+                            :cost cost})
+
+(def test-inventory-items
+  [(inventory-item "Widget" 28 5.00)
+   (inventory-item "Gizmo"  32 6.00)
+   (inventory-item "Thingy" 38 0.98)])
+
 
 (defn html-report [data]
   (println "<table>")
@@ -34,16 +36,19 @@
 (defmulti get-column-names type)
 (defmulti get-value (fn [this column-name] (type this)))
 
-(defmethod get-column-names ::InventoryItem [_]
-  ["description" "quantity"])
-
-(defmethod get-value ::InventoryItem [this column-name]
-  (get this (keyword column-name)))
-
 (defmethod get-column-names ::PurchaseOrder [_]
   ["id" "date" "amount"])
 
 (defmethod get-value ::PurchaseOrder [this column-name]
   (get this (keyword column-name)))
 
+(defmethod get-column-names ::InventoryItem [_]
+  ["description" "quantity" "unit cost" "total cost"])
+
+(defmethod get-value ::InventoryItem [this column-name]
+  (case column-name
+    "description" (:description this)
+    "quantity"    (:quantity this)
+    "unit cost"   (:cost this)
+    "total cost"  (* (:cost this) (:quantity this))))
 
